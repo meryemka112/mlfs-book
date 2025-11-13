bbd3830d33033de639bb6596ac49d22ffdd78ed0
meryemka112 <maryemkazdaghli112@gmail.com>
2025-11-13 15:07:33 +0100

    running all

--- mlfs/config.py
+++ mlfs/config.py
@@ -28,6 +28,10 @@ class HopsworksSettings(BaseSettings):
 
     # Air Quality
     AQICN_API_KEY: SecretStr | None = None
+    AQICN_COUNTRY: str | None = None
+    AQICN_CITY: str | None = None
+    AQICN_STREET: str | None = None
+    AQICN_URL: str | None = None
     
     # Other API Keys
     FELDERA_API_KEY: SecretStr | None = None    
